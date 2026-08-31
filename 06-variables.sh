9e6844b7684d9361b3b8dcb74f2134d9a9a05833
sitha ramisetti <sitha.r.ramisetti@accenture.com>
2026-08-31 21:59:49 +0530

    new variable

---
+++ 06-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+START_TIME=$(date+%s)
+echo "script executed start time is $START_TIME"
+
+sleep 10
+
+END_TIME=$(date+%s)
+
+TOTAL_TIME =$($START_TIME - $END_TIME)
+
+echo "script executed end time is $TOTAL_TIME SEC"
