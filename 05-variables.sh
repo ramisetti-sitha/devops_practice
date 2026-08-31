5095ce1babdfa7dc34863e3c9ef18c492894cfb2
sitha ramisetti <sitha.r.ramisetti@accenture.com>
2026-08-31 21:53:14 +0530

    new variable

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter username::"
+read USER_NAME
+
+echo "username is::$USER_NAME"
+
+echo "please enter your password::"
+read -s PASSWORD
+
+echo "password is::$PASSWORD"
