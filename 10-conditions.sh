004f80a34133796d93ddb0e04d089d61b24d953e
sitha ramisetti <sitha.r.ramisetti@accenture.com>
2026-09-01 20:14:14 +0530

    dt

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -4,8 +4,8 @@ num=$1
 
 if [ $num -gt 20 ]; then
    echo "given num is :: $num is greater than 20"
-elif
+elif [ $num -eq 20 ]; then
     echo "given num is:: $num is equal than 20"
 else
-    echo "given num is ::$num is less than 20"
+    echo "given num is :: $num is less than 20"
 fi
