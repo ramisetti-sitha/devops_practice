a8c803cce695e3b43edfedd9e12db4f0b3a93ed9
sitha ramisetti <sitha.r.ramisetti@accenture.com>
2026-09-01 22:16:46 +0530

    dt

--- 14.loops.sh
+++ 14.loops.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+
 USER_ID=$(id -u)
 logs_folder="/var/log/shell_script"
 logs_file="/var/log/shell_script/$0.log"
 
 if [ $USER_ID -ne 0 ]; then
-    echo "please get root access" |tee -a $logs_file
+    echo "$B please get root access $N" |tee -a $logs_file
     exit 1
 fi
 
 VALIDATE(){
 
     if [ $1 -ne 0 ]; then
-       echo "$2 installing..failure." |tee -a $logs_file
+       echo "$2 installing..$R failure $N." |tee -a $logs_file
        exit 1
     else
-       echo "$2 installing success...." |tee -a $logs_file
+       echo "$2 installing..$G success $N...." |tee -a $logs_file
     fi
 }
 
@@ -28,6 +33,6 @@ do
     dnf install $package -y &>> $logs_file
     VALIDATE $? $package
 else 
-    echo "$package is already installed,skipping"
+    echo "$package is already installed,$Y skipping $N"
 fi
 done
