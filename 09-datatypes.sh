27ec8c68b63292fd1b6f38f48bf9a0343d846eda
sitha ramisetti <sitha.r.ramisetti@accenture.com>
2026-09-01 19:54:11 +0530

    dt

---
+++ 09-datatypes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fruits=("apple","bannana","pomo")
+
+echo "fruits are:: ${fruits[@]}"
+echo "fruits are:: ${fruits[*]}"
+echo "first index:: ${fruits[0]}"
+echo "first index:: ${fruits[1]}"
+echo "first index:: ${fruits[2]}"
