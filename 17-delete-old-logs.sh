f8e02e18ebf166d8bb2749666732ec17ab239020
sitha ramisetti <sitha.r.ramisetti@accenture.com>
2026-09-07 20:20:09 +0530

    logs

---
+++ 17-delete-old-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+
+
+LOGS_DIR=
+LOGS_FILE="$LOGS_FILE/$0.log"
+
+if [ ! -d $LOGS_DIR ]; then
+     echo -e "$LOGS_DIR does not exist"
+     exit 1
+fi
+
+FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
+
+echo "$FILES_TO_DELETE"
